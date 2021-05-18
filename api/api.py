0d014efc41d95de7b95df67f8100b70e4fac6290
hchexy <lolturklol@gmail.com>
2021-05-18 18:27:05 +0100

    Testing with React/Flask

--- api/api.py
+++ api/api.py
@@ -1,8 +1,39 @@
 import time
 from flask import Flask
+from flask import request
+from flask import Flask
+from flask_cors import CORS
+import pandas as pd
+from pandas import DataFrame
+import os
+
 
 app = Flask(__name__)
 
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
+
+############ Genre API ############
+@app.route('/predict', methods = ['GET','POST'])
+def getPrediction():
+    g=[]
+    if request.method == 'GET':
+        return{'obj': "POSTONLY"}
+
+    g = request.form['symptoms']
+    print(g)
+    #print(l)
+    return {'list':g}
+#########################################
+@app.route('/symptoms', methods = ['GET'])
+def getSymptoms():
+    g=[]
+    path="../../LEI/Data/final.csv"
+    
+    print(path)
+    print("-----Bef---- ")
+    g = pd.read_csv(path).columns.to_numpy()[131:].tolist()
+    print(g)
+    #print(l)
+    return {'symptoms':g}
--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './CSS/App.css';
 import { Link } from "react-router-dom";
-
+import { render } from '@testing-library/react';
+import { Component, useState, useEffect } from 'react';
+const fs=require('fs')
 
 function App() {
+
+
+  var print=[<h1>Indx</h1>,<h3>+1</h3>,<h3>+1</h3>]
+  console.log("________--------CLIENT-------_________")
+
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -18,9 +26,19 @@ function App() {
       <div className="underName">
         <p>We recommend medical exams based on your symptoms</p>
       </div>
+      {<h3>111</h3>};
+      <button onClick={()=>fetch('/symptoms').then(res => res.json()).then(data => data.symptoms.forEach(e=><h4>2:{e}</h4>))} >ClickME</button>;
+      
+      <form method="POST" action="/predict">
+      <label for="symptoms">Symptoms</label>
+      <input type="text" id="symptoms" name="symptoms" multiple></input>
+      <input type="text" id="symptoms" name="symptoms" multiple></input>
+      <input type="submit" value="Submit"></input>
+      </form>
 
 
     </div>
+
   );
 }
 
